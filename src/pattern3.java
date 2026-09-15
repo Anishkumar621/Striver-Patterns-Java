6dfdcd84171d981adccd956cc4178a2f9c284e67
Anishkumar621 <anishkr0209@gmail.com>
2026-09-15 23:28:18 +0530

    Number Triangle Pattern

--- src/pattern3.java
+++ src/pattern3.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class Pattern1 {
+public class pattern3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n:");
-        int n = sc.nextInt();
+        System.out.println("Enter the value of n: ");
+        int n =sc.nextInt();
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<=i;j++)
             {
-                System.out.print("*" + " ");
+                System.out.print(j+1 + " ");
             }
             System.out.println(" ");
         }
