9070fb1c6e98b6426689b18b435f4c7307ee9d7a
Anishkumar621 <anishkr0209@gmail.com>
2026-09-16 20:52:26 +0530

    Striver Pattern 6 - Increasing Number Triangle

---
+++ src/pattern6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-i-1;j>=0;j--)
+            {
+                System.out.print(n-j +  " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
